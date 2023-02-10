7ffc40f951d780bdeed8cca09573a987dfcb60b4
Dilson José <dilsonjlrjr@gmail.com>
2023-02-10 14:35:45 -0300

    80

--- main.go
+++ main.go
@@ -38,6 +38,6 @@ func setupRouter() *gin.Engine {
 
 func main() {
 	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	// Listen and Server in 0.0.0.0:80
+	r.Run(":80")
 }
